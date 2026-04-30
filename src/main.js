7bb6494928d61d4c1a7ca06dc95dce8398ab58d8
paulinovart <pauline.dierick@gmail.com>
2026-04-30 09:10:42 +0200

    change map to classic

--- src/main.js
+++ src/main.js
@@ -100,16 +100,13 @@ const onDomLoaded = () => {
 
   //injection/chargement des tuiles
 
-  L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}",
-    {
-      minZoom: 1,
-      maxZoom: 16,
-      attribution:
-        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      ext: "jpg",
-    }
-  ).addTo(map);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
 
   getcountries();
 
